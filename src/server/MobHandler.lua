3882099949a7bb64c084ed26ded183d21d73895e
Aly <alcamara237@gmail.com>
2023-05-04 03:21:48 -0400

    Added a tag to every mob in the mobs folder. Updated the map asset

--- src/server/MobHandler.lua
+++ src/server/MobHandler.lua
@@ -11,16 +11,26 @@ local Zone = require(ReplicatedStorage.Shared.Zone)
 
 ----- Private Functions -----
 
+local function AddTag(mobPlaceholder, area, row, number)
+    CollectionService:AddTag(mobPlaceholder, area.."_"..row.."_"..number)
+end
+
 ----- Public Functions -----
 
 ----- Initialization -----
 module.Init = function()
-    local Area1MobsFolder = game.Workspace.Area1.Mobs
+    local mobsFolder = game.Workspace.Map.Mobs
 
-    for _, rowFolder in ipairs(Area1MobsFolder:GetChildren()) do
-        local mobRow = rowFolder.Name
-        for mobNumber, mobPlaceHolder in ipairs(rowFolder:GetChildren()) do
-            CollectionService:AddTag(mobPlaceHolder, "A1"..mobRow.."_"..mobNumber)
+    -- For every area folder in the mobs folder
+    for _, areaFolder in ipairs(mobsFolder:GetChildren()) do
+        local mobArea = areaFolder.Name
+        -- For every row folder in the mob folder
+        for _, rowFolder in ipairs(areaFolder:GetChildren()) do
+            local mobRow = rowFolder.Name
+            -- For every mob in the row folder
+            for mobNumber, mobPlaceholder in ipairs(rowFolder:GetChildren()) do
+                AddTag(mobPlaceholder, mobArea, mobRow, mobNumber)
+            end
         end
     end
 end
