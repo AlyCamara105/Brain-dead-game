921be548f64700c03c9c27eef03e2ccb57432d38
Aly <alcamara237@gmail.com>
2023-05-04 02:01:33 -0400

    The rojo is now working

--- src/server/init.server.lua
+++ src/server/init.server.lua
@@ -5,7 +5,5 @@ local ServerScriptService = game:GetService("ServerScriptService")
 ----- Loaded Modules -----
 
 local DataHandler = require(ServerScriptService.Server.DataHandler)
-local MobHandler = require(ServerScriptService.Server.MobHandler)
 
 DataHandler.Init()
-MobHandler.Init()
