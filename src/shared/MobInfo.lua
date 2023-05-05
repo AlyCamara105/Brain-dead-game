1f5ddea06952bbf6bafb6e1d5a424e206a18e570
Aly <alcamara237@gmail.com>
2023-05-05 01:01:22 -0400

    Fix styling

--- src/shared/MobInfo.lua
+++ src/shared/MobInfo.lua
@@ -3,17 +3,17 @@ local module = {}
 ----- Public Variables -----
 
 local MobInfo = {
-    Area1 = { { Health = 100, PremiumCurrency = 1 }, { Health = 150, PremiumCurrency = 2 } },
+	Area1 = { { Health = 100, PremiumCurrency = 1 }, { Health = 150, PremiumCurrency = 2 } },
 }
 
 ----- Public Functions -----
 
-module.GetMobInfo = function(mobTag) 
-    local tagSections = mobTag:split("_")
-    local tagArea = tagSections[1]
-    local tagRow = tagSections[2]
+module.GetMobInfo = function(mobTag)
+	local tagSections = mobTag:split("_")
+	local tagArea = tagSections[1]
+	local tagRow = tagSections[2]
 
-    return MobInfo[tagArea][tonumber(tagRow)]
+	return MobInfo[tagArea][tonumber(tagRow)]
 end
 
 return module
