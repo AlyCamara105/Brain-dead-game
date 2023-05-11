4abd2e17692db23453589978c39b58d9695c325a
Aly <alcamara237@gmail.com>
2023-05-11 12:46:48 -0400

    Moved the game to different places under the Dodo12w group

--- src/client/UIHandler.lua
+++ src/client/UIHandler.lua
@@ -16,19 +16,7 @@ local Player = Players.LocalPlayer
 ----- Public Function -----
 
 module.Init = function()
-	local UI = Roact.createElement("ScreenGui", {}, {
-		Frame = Roact.createElement("Frame", {
-			Size = UDim2.new(0, 200, 0, 300),
-		}, {
-			TextBox = Roact.createElement("TextBox", {
-				Size = UDim2.new(0, 200, 0, 300),
-                Text = "Type in /console into the chat to see the power unit data updating every second",
-                TextScaled = true
-			}),
-		}),
-	})
-
-	Roact.mount(UI, Player.PlayerGui)
+	
 end
 
 return module
